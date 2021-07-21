578b94b0657242749e07655374617135cee042a3
meijianxiao <1003409251@qq.com>
2021-07-21 15:09:35 +0800

    deploy

---
+++ precache-manifest.3914ed2a14eeae0acbca1ddf060f6c78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586e9766f6b89e8c69b52cfc2fd5f462",
+    "url": "/wangcai-website/1626837850.png"
+  },
+  {
+    "revision": "50c8cef164cf29246ef9",
+    "url": "/wangcai-website/css/app.5e485bea.css"
+  },
+  {
+    "revision": "2e54fb261a17d0d91c811ea973f322cb",
+    "url": "/wangcai-website/index.html"
+  },
+  {
+    "revision": "50c8cef164cf29246ef9",
+    "url": "/wangcai-website/js/app.cb3f8288.js"
+  },
+  {
+    "revision": "413dbae24f15371a4259",
+    "url": "/wangcai-website/js/chunk-vendors.bf68faf3.js"
+  },
+  {
+    "revision": "8743e3c6d3a9deb93a5a625838948152",
+    "url": "/wangcai-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wangcai-website/robots.txt"
+  }
+]);
